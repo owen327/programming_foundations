794a53a4d193155c899335e0ba06ffe6f4d58c56
Owen Tran <owen_327@hotmail.com>
2016-01-24 00:31:06 +0000

    Update circular_buffer

--- circular_buffer/circular_buffer.rb
+++ circular_buffer/circular_buffer.rb
@@ -3,28 +3,36 @@ class CircularBuffer
   class BufferFullException < StandardError; end
 
   def initialize(size)
-    @buffer = []
-    @max = size
+    @buffer = Array.new(size)
+    @start_index = 0
+    @end_index = 0
+    @size = size
   end
 
   def read
-    fail BufferEmptyException if @buffer.empty?
-    @buffer.shift
+    fail BufferEmptyException if @buffer.all?(&:nil?)
+    show = @buffer[@end_index]
+    @buffer[@end_index] = nil
+    @end_index += 1
+    @end_index = 0 if @end_index == @size
+    show
   end
 
   def write(element)
     return if element.nil?
-    fail BufferFullException if @buffer.size == @max
-    @buffer << element
+    fail BufferFullException if @buffer.all? { |elements| elements }
+    @buffer[@start_index] = element
+    @start_index += 1
+    @start_index = 0 if @start_index == @size
   end
 
   def clear
-    @buffer.clear
+    initialize(@size)
   end
 
   def write!(element)
     return if element.nil?
-    read if @buffer.size == @max
+    read if @buffer.all? { |elements| elements }
     write(element)
   end
 end
